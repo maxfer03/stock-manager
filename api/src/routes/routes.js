8908a8d99885922bcd89246bd91af00262d12651
Maximo Fernandez Arenas <sandsferdinand@gmail.com>
2021-08-29 17:02:19 -0300

    added basic routing

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,17 +1,34 @@
-import axios from 'axios';
-import { Router, response } from 'express';
+import axios from "axios";
+import { Router, response } from "express";
+import Prisma from "@prisma/client";
 /* import { v4 } from 'uuid'; */
 
+const prisma = new Prisma.PrismaClient();
 
 const path = Router();
 
-path.get("/items", (req, res) => {
-    return res.json("ITEMS")
-})
+path.get("/items", async (req, res) => {
+  const items = await prisma.item.findMany();
+  return res.json(items);
+});
+
+path.post("/addItem", async (req, res) => {
+  try {
+    const newItem = await prisma.item.create({
+      data: {
+        name: req.query.name.toLowerCase(),
+      },
+    });
+    console.table(newItem)
+    return res.json(newItem.name + " fue añadido al inventario");
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send("ERROR");
+  }
+});
 
 path.get("/types", (req, res) => {
-    return res.json("ITEM TYPES")
-})
+  return res.json("ITEM TYPES");
+});
 
-
-export default path
+export default path;
